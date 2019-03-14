49d5e8120eee2b64b5590dce8657a25fa94048af
phtn458 <phtn458@gmail.com>
2019-03-14 02:20:42 -0400

    first commit

---
+++ target/debug/app.d
@@ -0,0 +1,1 @@
+/mnt/c/Code/Rust/app/target/debug/app: /mnt/c/Code/Rust/app/src/main.rs
---
+++ target/debug/deps/app-3519eb536c139d8e.d
@@ -0,0 +1,5 @@
+/mnt/c/Code/Rust/app/target/debug/deps/app-3519eb536c139d8e: src/main.rs
+
+/mnt/c/Code/Rust/app/target/debug/deps/app-3519eb536c139d8e.d: src/main.rs
+
+src/main.rs:
